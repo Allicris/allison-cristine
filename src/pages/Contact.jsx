81f6e997be826c8c4059cb8eae9d0c57b52efbd8
Allison Serrano <allisonserrano171@yahoo.com>
2023-09-13 20:47:12 -0400

    working on form

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,5 +1,26 @@
+import { useState } from 'react';
+
 export default function Contact() {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+
+    alert(`Thank you ${firstName} ${lastName}`);
+    setFirstName('');
+    setLastName('');
+    setEmail('');
+    setMessage('');
+  };
+  
   return (
-    <p>A form will be inserted here</p>
+    <div >
+      <h1>
+        
+      </h1>
+    </div>
   );
 }
