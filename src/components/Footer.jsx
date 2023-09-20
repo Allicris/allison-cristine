ea306aa115ef8dd4c82efe9b375513d3e628b2ab
Allison Serrano <allisonserrano171@yahoo.com>
2023-09-20 18:42:09 -0400

    imported images

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,17 +1,20 @@
 import '../styles/Footer.css'
+import github from '../../public/images/github-icon.png';
+import linkedin from '../../public/images/linkedin-icon.png';
+import facebook from '../../public/images/facebook-icon.png';
 
 export default function Footer() {
   return (
     // <p> my links and icons will be here </p>
     <div className='icons'>
         <a href='https://github.com/Allicris'>
-          <img src='/public/images/github-icon.png'></img>
+          <img src={github}></img>
         </a>
         <a href='https://www.linkedin.com/feed/'>
-          <img src='/public/images/linkedin-icon.png'></img>
+          <img src={linkedin}></img>
         </a>
         <a href='https://www.facebook.com/allisonbabey'>
-        <img src='/public/images/facebook-icon.png'></img>
+        <img src={facebook}></img>
         </a>
       </div>
   );
