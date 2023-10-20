053b9965adbc215700904293cd1fb4b2d17aa1c2
Allison Serrano <allisonserrano171@yahoo.com>
2023-10-19 22:07:45 -0400

    added media queries and more content in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,21 +1,24 @@
 import '../styles/Footer.css'
 import github from '../../public/images/github-icon.png';
 import linkedin from '../../public/images/linkedin-icon.png';
-import facebook from '../../public/images/facebook-icon.png';
+import fiverr from '../../public/images/fiverr.png';
 
 export default function Footer() {
   return (
     // <p> my links and icons will be here </p>
+    <div>
     <div className='icons'>
-        <a href='https://github.com/Allicris'>
+        <a className='github' href='https://github.com/Allicris'>
           <img src={github}></img>
         </a>
         <a href='https://www.linkedin.com/feed/'>
           <img src={linkedin}></img>
         </a>
-        <a href='https://www.facebook.com/allisonbabey'>
-        <img src={facebook}></img>
+        <a className='fiverr' href='https://www.fiverr.com/allicris11?up_rollout=true'>
+          <img src={fiverr}></img>
         </a>
+      </div>
+      <p className='footer-text'> Developed by Allison Serrano with ❤️.</p>
       </div>
   );
 }
